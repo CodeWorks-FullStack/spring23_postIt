73986db23ac16a55a0950463ba00ae4b1908ca8b
Miles-Collins <miles.collins@ymail.com>
2023-04-04 12:30:03 -0600

    comments

--- spring23_postIt/server/services/AlbumsService.js
+++ spring23_postIt/server/services/AlbumsService.js
@@ -6,26 +6,32 @@ class AlbumsService{
   // SOFT DELETE ALBUMS BY FLIPPING BOOL ON IT. THIS WON'T GET RID OF IT PERMANENTLY IN THE DATABASE
   async archive(albumId, userId) {
     let album = await this.getOne(albumId)
+
     // NOTE MAKE SURE YOU'RE MAKING A VERIFY CHECK TO MAKE SURE THE PERSON TRYING TO "DELETE" THIS ALBUM IS ALSO THE PERSON WHO CREATED IT
     if(album.creatorId != userId) {
       throw new Forbidden(`You do not have permission to archive Album ${album.title}.`)
     }
+
     if(album.archived == true) {
       throw new Forbidden(`Album: ${album.title} has already been archived.`)
     }
+
     // NOTE MAKE SURE TO FLIP THE BOOLEAN, THEN AWAIT AND SAVE ALBUMS IN YOUR DATABASE
     album.archived = true
     await album.save()
+
     return `Album: ${album.title} was successfully deleted.`
   }
 
   // GETTING ALBUM BY ID
   async getOne(albumId) {
-    const album = await dbContext.Albums.findById(albumId).populate("creator", 'picture name')
-    // TODO MAKE SURE ALBUM ISN'T ARCHIVED, IF IS, THROW BAD REQUEST
+    const album = await dbContext.Albums.findById(albumId)
+    .populate("creator", 'picture name')
+
     if(album == null) {
       throw new BadRequest('This album does not exist.')
     }
+
     return album
   }
 
@@ -52,7 +58,7 @@ class AlbumsService{
   async getAlbumsImCollaboratedOn(accountId) {
     let albums = await dbContext.Collaborators.find({accountId})
     // NOTE WE'RE CALLING TO OUR "album" VIRTUAL
-    // NOTE THEN WE'RE TARGETING THAT SPECIFC ALBUM, AND POPULATING IT'S VIRTUALS ON TO THE ORIGINAL "album" VIRTUAL
+    // NOTE THEN WE'RE TARGETING THAT SPECIFIC ALBUM, AND POPULATING IT'S VIRTUALS ON TO THE ORIGINAL "album" VIRTUAL
     .populate({path: "album",
         populate: {
         path: "creator memberCount"
